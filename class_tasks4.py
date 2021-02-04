20b8c435574e468128992e83a98a3b574c3bd030
LoganGamble420 <77423973+LoganGamble420@users.noreply.github.com>
2021-02-03 16:50:35 -1000

    Add files via upload

---
+++ class_tasks4.py
@@ -0,0 +1,78 @@
+#2.1
+#1. int float bool complex
+#2 print(3*8.7) 
+#3 the answer to number two is a float 
+#4 int(5.67) 
+#5 A * B would go first, then the answer of that would be subtracted by C
+
+#2.2
+#1 true division divides the two numbers as floats and gives the exact decimal answer. Floor division rounds to the  in front of the decimal point
+#2 it rounds the number down to the nearest integer
+#3 5
+#4 (a) is raising (B/C) to the power of A and (b) is raising B to the power of A, and then dividing the answer by C 
+#5 round(a/b)
+
+#2.3
+#1 This means 4 < 5 which would be True
+#2 this means 4 not equal to 4 which would be False
+#3 this means 2 is equal to 2 which would be True
+#4 1 < 3 < 5.0 which would be true
+#5 1 < 2 is True and 2 > 1 which is True
+
+#2.4
+#1 when they are assigned a data type
+#2 after the variable is created
+#3 a-b
+#4 because c is not a variable
+#5 yes you can chnage the value of a variable at anytime
+
+#3.1
+#1 string, lists, tuples, 
+#2 name = 'logan' print(name)
+#3 single quotes for single words symbol-like strings and double quotes around natural language 
+#4 mostly for style reasons
+#5 when you declare integer 5 as a variable declare it in string format like '5'
+
+#3.2
+#1 to show where the escape sequence starts
+#2 \t
+#3 \n
+#4 print("Blue\nGreen\nOrange\n")
+#5 print("Orange\tApple\tBanana")
+
+#3.3 
+#1 r
+#2 raw strings treat characters such as backslashes as a character instead of an operator
+#3 
+#4 docstrings and spanning multiple lines
+#5 'Logan' + 'Gamble' join('Logan','Gamble')
+
+#3.4
+#1 animal='Gorilla'
+#2 print(animal[0])
+#3 print(animal[len(animal)-1])
+#4 n=0
+#  print(animal[n])
+#  while n != len(animal)-1:
+#    if n % 2 == 0:
+#        n = n + 1
+#        print(animal[n])
+#    else:
+#        n = n + 1
+#5 animal.replace('l', 'b')
+
+#4.1
+#1 it is the ability for one variable to switch data types
+#2 it means that the variable can take many forms
+#3 it will say cute is the object, then it will declare dog as the varibale. Finally, it will link the variable and object
+#4 variables - entries in a system with space for links to objects
+#    objects -  pices of allocated memory with enough space to represent themselves with values
+#   References - auto pointers from variable to objects
+#5 When a variable is assigned to a new object the old object will never be used again and is deleted from the memory#6 yes that is possible and common
+#7 a will be callable under 6 and under 'lamp' given context, but 'lamp' overides the number 6
+#8 if the value of an object can change it is mutable, otherwise immutable
+#9 int float string
+#10 265
+#11 it tells you the reference count of an object
+#12 in the context of python it is when a function permits entities of different data types to be called upon 
+
